e69bdcb95f7e17ed0a1572d6218786345918bfdb
itzayush21 <itzayush21@gmail.com>
2024-12-22 12:25:05 +0530

    publish Changes

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'QApplication',
+    version= '0.0.1',
+    author= 'Ayush singh',
+    author_email= 'itzayush21@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
